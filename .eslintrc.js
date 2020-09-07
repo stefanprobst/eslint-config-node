a7881e313c610c9a317641cacd582790752aba2c
Stefan Probst <stefanprobst@kolabnow.com>
2020-09-07 09:52:53 +0200

    feat: add jest style rules, prefer url globals in node

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,19 @@ module.exports = {
   root: true,
   env: {
     es2020: true,
-    jest: true,
+    "jest/globals": true,
     node: true,
   },
   extends: [
     "eslint:recommended",
     "plugin:jest/recommended",
+    "plugin:jest/style",
     "plugin:node/recommended",
     "plugin:prettier/recommended",
   ],
+  rules: {
+    "node/prefer-global/url": ["warn"],
+    "node/prefer-global/url-search-params": ["warn"],
+    "node/prefer-promises/fs": ["warn"],
+  },
 };
