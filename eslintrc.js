00f6672b9d553faf85fcffcbab84a311eadc47a9
Stefan Probst <stefanprobst@kolabnow.com>
2020-09-08 09:42:08 +0200

    feat: add eslint-plugin-import

--- eslintrc.js
+++ eslintrc.js
@@ -7,12 +7,19 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
     "plugin:jest/recommended",
     "plugin:jest/style",
     "plugin:node/recommended",
     "plugin:prettier/recommended",
   ],
   rules: {
+    "import/order": [
+      "error",
+      { alphabetize: { order: "asc", caseInsensitive: true } },
+    ],
+    "import/no-anonymous-default-export": "error",
     "node/prefer-global/url": ["warn"],
     "node/prefer-global/url-search-params": ["warn"],
     "node/prefer-promises/fs": ["warn"],
