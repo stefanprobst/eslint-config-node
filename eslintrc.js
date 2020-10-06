21b03f983317870c6f67e5274f1099abafba1b9a
Stefan Probst <stefanprobst@kolabnow.com>
2020-10-06 11:27:20 +0200

    feat: add ts strict-boolean-expressions rule

--- eslintrc.js
+++ eslintrc.js
@@ -2,72 +2,73 @@ module.exports = {
   root: true,
   env: {
     es2020: true,
-    'jest/globals': true,
+    "jest/globals": true,
     node: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:jest/recommended',
-    'plugin:jest/style',
-    'plugin:node/recommended',
-    'plugin:prettier/recommended',
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+    "plugin:node/recommended",
+    "plugin:prettier/recommended",
   ],
   rules: {
-    'no-restricted-globals': [
-      'error',
+    "no-restricted-globals": [
+      "error",
       {
-        name: 'isNaN',
-        message: 'Use Number.isNaN instead.',
+        name: "isNaN",
+        message: "Use Number.isNaN instead.",
       },
     ],
-    'import/order': [
-      'error',
-      { alphabetize: { order: 'asc', caseInsensitive: true } },
+    "import/order": [
+      "error",
+      { alphabetize: { order: "asc", caseInsensitive: true } },
     ],
-    'import/no-anonymous-default-export': 'error',
-    'node/prefer-global/url': ['warn'],
-    'node/prefer-global/url-search-params': ['warn'],
-    'node/prefer-promises/fs': ['warn'],
+    "import/no-anonymous-default-export": "error",
+    "node/prefer-global/url": ["warn"],
+    "node/prefer-global/url-search-params": ["warn"],
+    "node/prefer-promises/fs": ["warn"],
   },
   overrides: [
     {
-      files: ['*.ts'],
+      files: ["*.ts"],
       extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:import/typescript',
-        'prettier/@typescript-eslint',
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/typescript",
+        "prettier/@typescript-eslint",
       ],
       settings: {
-        'import/resolver': {
+        "import/resolver": {
           typescript: {
             alwaysTryTypes: true,
-            project: './tsconfig.json',
+            project: "./tsconfig.json",
           },
         },
         node: {
-          tryExtensions: ['.js', '.json', '.node', '.ts'],
+          tryExtensions: [".js", ".json", ".node", ".ts"],
         },
       },
       rules: {
-        'node/no-missing-import': [
-          'error',
+        "@typescript-eslint/strict-boolean-expressions": "error",
+        "node/no-missing-import": [
+          "error",
           {
-            resolvePaths: ['node_modules/@types'],
+            resolvePaths: ["node_modules/@types"],
           },
         ],
-        'node/no-unsupported-features/es-syntax': [
-          'error',
+        "node/no-unsupported-features/es-syntax": [
+          "error",
           {
-            ignores: ['modules'],
+            ignores: ["modules"],
           },
         ],
-        'node/shebang': [
-          'error',
+        "node/shebang": [
+          "error",
           {
             convertPath: {
-              'src/**/*.ts': ['^src/(.+?)\\.ts$', 'dist/$1.js'],
+              "src/**/*.ts": ["^src/(.+?)\\.ts$", "dist/$1.js"],
             },
           },
         ],
