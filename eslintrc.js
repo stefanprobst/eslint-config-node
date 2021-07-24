f9cfb722550007ec96124095f3e9bd2170763802
Stefan Probst <stefanprobst@kolabnow.com>
2021-07-24 09:23:50 +0200

    move plugins to deps

--- eslintrc.js
+++ eslintrc.js
@@ -7,14 +7,25 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
+    "plugin:import/recommended",
     "plugin:jest/recommended",
     "plugin:jest/style",
     "plugin:node/recommended",
-    "plugin:prettier/recommended",
+    "prettier",
   ],
   rules: {
+    "arrow-body-style": [
+      "error",
+      "as-needed",
+      {
+        requireReturnForObjectLiteral: true,
+      },
+    ],
+    "consistent-return": "error",
+    eqeqeq: ["error", "always", { null: "ignore" }],
+    "no-console": "warn",
+    "no-implicit-coercion": "error",
+    "no-param-reassign": "error",
     "no-restricted-globals": [
       "error",
       {
@@ -22,11 +33,24 @@ module.exports = {
         message: "Use Number.isNaN instead.",
       },
     ],
+    "no-throw-literal": "error",
+    "no-var": "error",
+    "prefer-const": "error",
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-anonymous-default-export": "error",
+    "import/no-duplicates": "error",
     "import/order": [
       "error",
-      { alphabetize: { order: "asc", caseInsensitive: true } },
+      {
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
     ],
-    "import/no-anonymous-default-export": "error",
+
     "node/prefer-global/url": ["warn"],
     "node/prefer-global/url-search-params": ["warn"],
     "node/prefer-promises/fs": ["warn"],
@@ -50,7 +74,24 @@ module.exports = {
         },
       },
       rules: {
+        "@typescript-eslint/array-type": [
+          "error",
+          {
+            default: "generic",
+          },
+        ],
+        "@typescript-eslint/consistent-type-imports": "error",
+        "@typescript-eslint/naming-convention": "warn",
+        "@typescript-eslint/no-unnecessary-condition": "error",
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          {
+            argsIgnorePattern: "^_",
+            varsIgnorePattern: "^_",
+          },
+        ],
         "@typescript-eslint/strict-boolean-expressions": "error",
+        "@typescript-eslint/switch-exhaustiveness-check": "error",
         "node/no-missing-import": [
           "error",
           {
